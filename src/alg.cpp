6cbb4545d7024cea9d9555b5503e2378a92c9a91
TailorOfEnbizaka <160338576+TailorOfEnbizaka@users.noreply.github.com>
2024-02-26 18:04:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,17 @@
-// Copyright 2021 NNTU-CS
+#include <iostream>
+#include "alg.h"
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    int nod;
+    for (int i = 1; i <= a; i++)
+    {
+        if ((a % i == 0) && (b % i == 0))
+            nod = i;
+    }
+    return nod;
+}
+
+int main() {
+    std::cout << gcd(10, 15) << std::endl;
+    return 0;
 }
